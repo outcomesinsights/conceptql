54fd8455032fa419a7aa36c1ba39e2198cc0326e
Ryan Duryea <aguynamedryan@gmail.com>
2014-08-19 17:05:13 -0700

    Complement: fix bugs intro'd by 5 column changes

--- lib/conceptql/nodes/complement.rb
+++ lib/conceptql/nodes/complement.rb
@@ -9,6 +9,7 @@ module ConceptQL
           positive_query = db.from(child.evaluate(db))
             .select(:criterion_id)
             .exclude(:criterion_id => nil)
+            .where(:criterion_type => type.to_s)
           query = db.from(make_table_name(type))
             .exclude(type_id(type) => positive_query)
           db.from(select_it(query, type))
@@ -16,6 +17,19 @@ module ConceptQL
           union_query.union(q, all: true)
         end
       end
+
+=begin
+This is an alternate, but equally accurate way to do complement.
+We'll need to benchmark which is faster.
+      def query2(db)
+        child = children.first
+        froms = child.types.map do |type|
+          select_it(db.from(make_table_name(type)), type)
+        end
+        big_from = froms.inject { |union_query, q| union_query.union(q, all:true) }
+        db.from(big_from).except(child.evaluate(db))
+      end
+=end
     end
   end
 end
