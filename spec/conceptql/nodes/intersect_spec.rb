eb735bf15a058ba7f8e6a2ebe50693cf1bae9668
Ryan Duryea <aguynamedryan@gmail.com>
2014-08-28 17:45:29 -0700

    spec: fix up a few specs regard text/date casting

--- spec/conceptql/nodes/intersect_spec.rb
+++ spec/conceptql/nodes/intersect_spec.rb
@@ -13,7 +13,7 @@ describe ConceptQL::Nodes::Intersect do
       double2 = QueryDouble.new(2)
       double3 = QueryDouble.new(3)
       double1.must_behave_like(:evaluator)
-      ConceptQL::Nodes::Intersect.new(double1, double2, double3).query(Sequel.mock).sql.must_equal "SELECT * FROM (SELECT * FROM (SELECT * FROM table1 INTERSECT ALL SELECT * FROM table2) AS t1 INTERSECT ALL SELECT * FROM table3) AS t1"
+      ConceptQL::Nodes::Intersect.new(double1, double2, double3).query(Sequel.mock).sql.must_equal "SELECT * FROM (SELECT * FROM (SELECT * FROM table1 INTERSECT SELECT * FROM table2) AS t1 INTERSECT SELECT * FROM table3) AS t1"
     end
 
     it 'works for multiple criteria of different type' do
@@ -21,7 +21,7 @@ describe ConceptQL::Nodes::Intersect do
       double2 = QueryDouble.new(2, :person)
       double3 = QueryDouble.new(3)
       double1.must_behave_like(:evaluator)
-      ConceptQL::Nodes::Intersect.new(double1, double2, double3).query(Sequel.mock).sql.must_equal "SELECT * FROM (SELECT * FROM (SELECT * FROM table1 INTERSECT ALL SELECT * FROM table3) AS t1 UNION ALL SELECT * FROM table2) AS t1"
+      ConceptQL::Nodes::Intersect.new(double1, double2, double3).query(Sequel.mock).sql.must_equal "SELECT * FROM (SELECT * FROM (SELECT * FROM table1 INTERSECT SELECT * FROM table3) AS t1 UNION ALL SELECT * FROM table2) AS t1"
     end
 
     it 'works for single criteria' do
