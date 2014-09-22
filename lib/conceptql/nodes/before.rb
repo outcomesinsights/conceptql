0e597265cb7eb207b2693faeabe2765b7e43e1aa
Ryan Duryea <aguynamedryan@gmail.com>
2014-09-22 15:50:43 -0700

    Before: Reduce right stream to one result per person

--- lib/conceptql/nodes/before.rb
+++ lib/conceptql/nodes/before.rb
@@ -3,6 +3,10 @@ require_relative 'temporal_node'
 module ConceptQL
   module Nodes
     class Before < TemporalNode
+      def right_stream(db)
+        right.evaluate(db).from_self.group_by(:person_id).select(:person_id, Sequel.function(:max, :start_date).as(:start_date)).as(:r)
+      end
+
       def where_clause
         Proc.new { l__end_date < r__start_date }
       end
