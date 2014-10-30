ea2ad73aac36dc1f952ba0a95ed704580f9b9ccd
Ryan Duryea <aguynamedryan@gmail.com>
2014-10-30 14:36:14 -0700

    ObservationPeriod: add this casting node

---
+++ lib/conceptql/nodes/observation_period.rb
@@ -0,0 +1,26 @@
+require_relative 'casting_node'
+
+module ConceptQL
+  module Nodes
+    class ObservationPeriod < CastingNode
+      def my_type
+        :observation_period
+      end
+
+      def i_point_at
+        []
+      end
+
+      def these_point_at_me
+        # I could list ALL the types we use, but the default behavior of casting,
+        # when there is no explicit casting defined, is to convert everything to
+        # person IDs
+        #
+        # So by defining no known castable relationships in this node, all
+        # types will be converted to person
+        []
+      end
+    end
+  end
+end
+
