761f78e70d60c9b6607bdfb4cf7cb6799af3f314
Ryan Duryea <aguynamedryan@gmail.com>
2014-11-15 21:22:35 -0800

    FromSeerVisits: fix bug in query

--- lib/conceptql/nodes/from_seer_visits.rb
+++ lib/conceptql/nodes/from_seer_visits.rb
@@ -10,7 +10,7 @@ module ConceptQL
         visit_ids = stream.evaluate(db)
           .from_self
           .where(criterion_type: 'visit_occurrence')
-        query = db[:observation].where(visit_occurrence_id: visit_ids)
+        query = db[:observation].where(visit_occurrence_id: visit_ids.select(:criterion_id))
         arguments.inject(query) do |q, key|
           q.where(observation_source_value: key.to_s.upcase)
         end
