0ede5e951cf01b9057d447c0a7baa58944aea95c
Ryan Duryea <aguynamedryan@gmail.com>
2014-12-03 17:57:01 -0800

    Filter: start matching on person_id
    If we don't, and for some reason our data is awful and has duplicate
    criterion_ids, we get incorrect results

--- lib/conceptql/nodes/filter.rb
+++ lib/conceptql/nodes/filter.rb
@@ -5,10 +5,10 @@ module ConceptQL
     class Filter < BinaryOperatorNode
       def query(db)
         rhs = right.evaluate(db)
-        rhs = rhs.select_group(:criterion_id, :criterion_type)
+        rhs = rhs.select_group(:person_id, :criterion_id, :criterion_type)
         query = db.from(Sequel.as(left.evaluate(db), :l))
         query = query
-          .left_join(Sequel.as(rhs, :r), l__criterion_id: :r__criterion_id, l__criterion_type: :r__criterion_type)
+          .left_join(Sequel.as(rhs, :r), l__person_id: :r__person_id, l__criterion_id: :r__criterion_id, l__criterion_type: :r__criterion_type)
           .exclude(r__criterion_id: nil)
           .select_all(:l)
         db.from(query)
