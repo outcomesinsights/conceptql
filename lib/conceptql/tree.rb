89fa9013ef9bc0074da2c90d58b784d926d3405a
Ryan Duryea <aguynamedryan@gmail.com>
2015-01-12 14:30:33 -0800

    Tree#temp_tables: add temp_table tracking to Tree

--- lib/conceptql/tree.rb
+++ lib/conceptql/tree.rb
@@ -3,12 +3,13 @@ require 'active_support/core_ext/hash'
 
 module ConceptQL
   class Tree
-    attr :nodifier, :behavior, :defined, :opts
+    attr :nodifier, :behavior, :defined, :opts, :temp_tables
     attr_accessor :person_ids
     def initialize(opts = {})
       @nodifier = opts.fetch(:nodifier, Nodifier.new)
       @behavior = opts.fetch(:behavior, nil)
       @defined = {}
+      @temp_tables = {}
       @opts = {}
     end
 
