7a22713a1127e139ac5d391b5d08a68233fd7374
Ryan Duryea <aguynamedryan@gmail.com>
2015-02-23 13:57:12 -0800

    Node: fix bug that prevented many tests from running

--- lib/conceptql/nodes/node.rb
+++ lib/conceptql/nodes/node.rb
@@ -42,7 +42,7 @@ module ConceptQL
       def select_it(query, specific_type = nil)
         specific_type = type if specific_type.nil? && respond_to?(:type)
         q = query.select(*columns(query, specific_type))
-        if tree.person_ids && children.empty?
+        if tree && tree.person_ids && children.empty?
           q = q.where(person_id: tree.person_ids).from_self
         end
         q
