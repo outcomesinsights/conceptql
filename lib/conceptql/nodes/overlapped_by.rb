4784ebde80465a8c7f46bf8242e55a824dbe4ce2
Ryan Duryea <aguynamedryan@gmail.com>
2015-03-09 17:30:02 -0700

    OverlappedBy: make query actually work right

--- lib/conceptql/nodes/overlapped_by.rb
+++ lib/conceptql/nodes/overlapped_by.rb
@@ -4,12 +4,11 @@ module ConceptQL
   module Nodes
     class OverlappedBy < TemporalNode
       def where_clause
-        clauses = []
-        clauses << [Proc.new { r__start_date <= l__start_date}, Proc.new { l__start_date <= r__end_date }, Proc.new { r__end_date <= l__end_date }]
         if inclusive?
-          clauses << [Proc.new { r__start_date <= l__start_date}, Proc.new { l__end_date <= r__end_date }]
+          [Proc.new { r__start_date <= l__start_date}, Proc.new { l__start_date <= r__end_date }]
+        else
+          [Proc.new { r__start_date <= l__start_date}, Proc.new { l__start_date <= r__end_date }, Proc.new { r__end_date <= l__end_date }]
         end
-        clauses.map { |clause| clause.map { |pr| Sequel.expr(pr) }.inject(&:&) }.inject(&:|)
       end
     end
   end
