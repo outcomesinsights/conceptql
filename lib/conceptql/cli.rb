5a1e3224a8a767eb85e706f329c619c3b7a71182
Ryan Duryea <aguynamedryan@gmail.com>
2016-03-29 13:57:43 -0700

    Create metadata.js as expected by JAV/JAM tests

--- lib/conceptql/cli.rb
+++ lib/conceptql/cli.rb
@@ -75,7 +75,7 @@ module ConceptQL
 
     desc 'metadata', 'Generates the metadata.js file for the JAM'
     def metadata
-      File.write('metadata.js', "var metadata = #{ConceptQL.metadata(warn: true).to_json};")
+      File.write('metadata.js', "$metadata = #{ConceptQL.metadata(warn: true).to_json};")
       File.write('metadata.json', ConceptQL.metadata.to_json)
       if system("which", "json_pp", out: File::NULL, err: File::NULL)
         system("cat metadata.json | json_pp", out: "metadata_pp.json")
