33c1438cf3f07c97f9e93e3aa7435f1029e6387f
Ryan Duryea <aguynamedryan@gmail.com>
2016-04-18 17:23:38 -0700

    OneInTwoOut: update to use new parameters/behavior

--- lib/conceptql/operators/one_in_two_out.rb
+++ lib/conceptql/operators/one_in_two_out.rb
@@ -27,43 +27,90 @@ in an inpatient setting
       validate_required_options :outpatient_minimum_gap
       validate_option DateAdjuster::VALID_INPUT, :outpatient_minimum_gap, :outpatient_maximum_gap
 
+      default_query_columns
+
       def query(db)
-        ds = visit_query(db).clone(:force_columns=>table_columns(:visit_occurrence))
-        inpatient = select_it(ds.where(place_of_service_concept_id: 8717), :visit_occurrence).from_self
-        outpatient = select_it(ds.exclude(place_of_service_concept_id: 8717), :visit_occurrence).from_self
-
-        date_diff = if db.database_type == :impala
-          Sequel.function(:datediff, Sequel.function(:max, :start_date), Sequel.function(:min, :end_date))
-        else
-          Sequel.function(:max, :start_date) - Sequel.function(:min, :end_date)
-        end
-
-        gap = options[:gap] || 30
-        valid_outpatient_people = outpatient
-          .select_group(:person_id)
-          .select_append(date_diff.as(:date_diff))
-          .from_self
-            .where{ date_diff() >= gap}
-
-        relevant_outpatient = outpatient.where(person_id: valid_outpatient_people.select(:person_id))
-        earliest(db, inpatient.union(relevant_outpatient))
-      end
-
-      def query_cols
-        SELECTED_COLUMNS + [:rn]
+        faked_out = FakeOperator.new(nodifier,
+          inpatient_events(db).union(outpatient_events(db).from_self),
+          stream.domains)
+        First.new(nodifier, faked_out).query(db)
       end
 
       private
-      def visit_query(db)
-        VisitOccurrence.new(nodifier, FakeOperator.new(nodifier, stream.evaluate(db).from_self, stream.domains)).query(db)
+
+      def inpatient_events(db)
+        q = db[marked_events(db)]
+              .where(type_id: inpatient_type_ids(db))
+              .or(type_id: 0)
+
+        unless options[:inpatient_length_of_stay].nil? || options[:inpatient_length_of_stay].zero?
+          q = q.where{ |o| Sequel.date_sub(o.end_date, o.start_date) > options[:inpatient_length_of_stay] }
+        end
+
+        if options[:inpatient_return_date] == 'Admit Date'
+          q = q.select(*(query_cols - [:end_date])).select_append(:start_date___end_date)
+        else
+          q = q.select(*(query_cols - [:start_date])).select_append(:end_date___start_date)
+        end
+
+        q.select(*SELECTED_COLUMNS)
       end
 
-      def earliest(db, query)
-        cte_name = scope.add_extra_cte(:earliest,
-            query.select_append { |o| o.row_number(:over, partition: :person_id, order: [Sequel.asc(:start_date), :criterion_domain, :criterion_id]){}.as(:rn) })
-        db[cte_name]
-          .from_self
-          .where(rn: 1)
+      def inpatient_type_ids(db)
+        db[:concept___c]
+          .join(:vocabulary___v, c__vocabulary_id: :v__vocabulary_id)
+          .grep(:v__vocabulary_name, '% type', case_insensitive: true)
+          .grep(:c__concept_name, '%inpatient%', case_insensitive: true)
+          .select(:c__concept_id)
+      end
+
+      def outpatient_events(db)
+        @outpat ||= scope.add_extra_cte(
+          :outpat_events,
+          db[marked_events(db)]
+            .exclude(type_id: inpatient_type_ids(db))
+        )
+
+        min_gap = options[:outpatient_minimum_gap] || 30
+        max_gap = options[:outpatient_maximum_gap] || 0
+
+        q = db[@outpat].from_self(alias: :o1)
+              .join(db[@outpat].as(:o2), o1__person_id: :o2__person_id)
+              .exclude(o1__criterion_id: :o2__criterion_id, o1__criterion_domain: :o2__criterion_domain)
+
+        if min_gap > 0
+          q = q.where { o2__start_date >= o1__start_date + min_gap }
+        end
+
+        if max_gap > 0
+          q = q.where { o2__start_date <= o1__start_date + max_gap }
+        end
+
+        if options[:outpatient_event_to_return] != 'Initial Event'
+          q = q.select_all(:o2)
+        else
+          q = q.select_all(:o1)
+        end
+
+        faked_out = FakeOperator.new(nodifier, q.from_self, stream.domains)
+        First.new(nodifier, faked_out).query(db).select(*SELECTED_COLUMNS)
+      end
+
+      def marked_events(db)
+        # Just a heads up that I'm doing something sneaky in this line: I'm
+        # asking the join to only happen if the criterion_domain of the row is
+        # condition_occurrence
+        #
+        # Under PostgreSQL, this seems to still return all rows on the left,
+        # but seems to avoid joining against condtion_occurrence unless the
+        # row itself is a condition_occurrence
+        @marked_events ||= scope.add_extra_cte(:in_out_events,
+            stream.evaluate(db)
+              .from_self(alias: :l)
+              .left_join(:condition_occurrence___c, { c__condition_occurrence_id: :l__criterion_id, l__criterion_domain: 'condition_occurrence' })
+              .select_all(:l)
+              .select_append(Sequel.function(:coalesce, :c__condition_type_concept_id, 0).as(:type_id))
+          )
       end
 
       class FakeOperator < Operator
--- test/operators/one_in_two_out_test.rb
+++ test/operators/one_in_two_out_test.rb
@@ -3,8 +3,15 @@ require_relative '../helper'
 describe ConceptQL::Operators::OneInTwoOut do
   it "should produce correct results" do
     criteria_ids(
-      [:one_in_two_out, [:icd9, "412"], {:gap=>30, :blah=>true}]
-    ).must_equal("visit_occurrence"=>[757, 2705, 3847, 4378, 6640, 7810, 8108, 8806, 9544, 10786, 10894, 11198, 11412, 11783, 12370, 13783, 13972])
+      [:one_in_two_out, [:icd9, "412"], {:min_gap=>30, :blah=>true}]
+    ).must_equal("condition_occurrence"=>[1829, 6083, 8618, 9882, 15149, 17774, 18412, 20005, 21619, 24437, 24707, 25309, 25888, 26766, 28188, 31542, 31877])
+  end
+
+  it "should treat non-conditions as inpatient" do
+    criteria_ids(
+      [:one_in_two_out, [:hcpcs, 'A0382'] , {:min_gap=>30}]
+    ).must_equal("procedure_occurrence"=>[2706, 7446, 31137])
+
   end
 
   it "should handle errors when annotating" do
