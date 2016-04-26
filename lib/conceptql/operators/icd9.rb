243e7964d23d08d59488effd417d20744dad83b7
Ryan Duryea <aguynamedryan@gmail.com>
2016-04-26 09:48:18 -0700

    Correct formatting of ICD-9-CM

--- lib/conceptql/operators/icd9.rb
+++ lib/conceptql/operators/icd9.rb
@@ -5,7 +5,7 @@ module ConceptQL
     class Icd9 < ConditionOccurrenceSourceVocabularyOperator
       register __FILE__, :omopv4
 
-      preferred_name 'ICD-9 CM'
+      preferred_name 'ICD-9-CM'
       desc 'Searches the condition_occurrence table for the given set of ICD-9 codes.'
       argument :icd9s, type: :codelist, vocab: 'ICD9CM'
       predominant_domains :condition_occurrence
