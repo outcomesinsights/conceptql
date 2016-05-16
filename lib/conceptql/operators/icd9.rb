f615e2a527ea8ada9bad6c607d9fbb5994d69b62
Ryan Duryea <aguynamedryan@gmail.com>
2016-05-16 16:48:31 -0700

    ICD-9-CM => ICD-9 CM

--- lib/conceptql/operators/icd9.rb
+++ lib/conceptql/operators/icd9.rb
@@ -5,7 +5,7 @@ module ConceptQL
     class Icd9 < ConditionOccurrenceSourceVocabularyOperator
       register __FILE__, :omopv4
 
-      preferred_name 'ICD-9-CM'
+      preferred_name 'ICD-9 CM'
       argument :icd9s, type: :codelist, vocab: 'ICD9CM'
       predominant_domains :condition_occurrence
 
