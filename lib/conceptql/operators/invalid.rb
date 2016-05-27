97f0eb706d2f744776ee890275c25e74d54d217b
Ryan Duryea <aguynamedryan@gmail.com>
2016-05-27 15:49:36 -0700

    Invalid: raise error when #query is called

--- lib/conceptql/operators/invalid.rb
+++ lib/conceptql/operators/invalid.rb
@@ -15,6 +15,10 @@ module ConceptQL
         @operator_name ||= arguments.shift.to_s || "invalid"
       end
 
+      def query(db)
+        raise "Invalid#query called.  #{errors}"
+      end
+
       def annotate(db)
         if options[:left] || options[:right]
           options[:left] = to_op(options[:left]).annotate(db) if options[:left]
