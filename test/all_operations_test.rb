9ee1a8f3d9ed1674db8267ac158c249876502a92
Ryan Duryea <aguynamedryan@gmail.com>
2016-07-07 12:11:39 -0700

    retab some bad whitespace

--- test/all_operations_test.rb
+++ test/all_operations_test.rb
@@ -1,34 +1,34 @@
 require_relative './helper'
 
-describe ConceptQL::Operators do 
+describe ConceptQL::Operators do
 
   Dir['./test/statements/**/*'].each do |f|
-  	next if File.directory? f
+    next if File.directory? f
     f.slice! './test/statements/'
     basename = File.basename(f)
 
-    it "should produce correct results for #{f}" do 
+    it "should produce correct results for #{f}" do
       test_type = basename.split("_").first
 
       case test_type
       when "crit"
-      	criteria_ids(f)
+        criteria_ids(f)
       when "anno"
-      	annotate(f)
+        annotate(f)
       when "count"
-      	count(f)
+        criteria_counts(f)
       when "scanno"
-      	scope_annotate(f)
+        scope_annotate(f)
       when "domains"
-      	domains(f)
+        domains(f)
       when "cc"
-      	criteria_counts(f)
+        criteria_counts(f)
       when "optcc"
-      	optimized_criteria_counts(f)
+        optimized_criteria_counts(f)
       when "num"
-      	numeric_values(f)
-      else 
-      	raise "Invalid operation test prefix: #{test_type}"
+        numeric_values(f)
+      else
+        raise "Invalid operation test prefix: #{test_type}"
       end
     end
   end
