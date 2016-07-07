8074589bd795b79355350a2a6a5b10db7134c6da
Ben Hannel <bhannel@stanford.edu>
2016-07-06 17:44:21 -0700

    Changed "numeric_" prefix to "num_" for consistency

--- test/operators/numeric_test.rb
+++ test/operators/numeric_test.rb
@@ -2,15 +2,15 @@ require_relative '../helper'
 
 describe ConceptQL::Operators::Numeric do
   it "should produce correct results" do
-    numeric_values("numeric/numeric_values_1",
+    numeric_values("numeric/num_values_1",
       [:numeric, 1]
     )
 
-    numeric_values("numeric/numeric_values_2",
+    numeric_values("numeric/num_values_2",
       [:numeric, 1, [:icd9_procedure, "00.13"]]
     )
 
-    numeric_values("numeric/numeric_values_3",
+    numeric_values("numeric/num_values_3",
       [:numeric, :criterion_id, [:icd9_procedure, "00.13"]]
     )
   end
