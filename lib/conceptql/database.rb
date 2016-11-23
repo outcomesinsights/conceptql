6a094a32c8b548985d160b5fc6b3a202e80b412a
Ryan Duryea <aguynamedryan@gmail.com>
2016-11-22 16:43:07 -0800

    temp save

--- lib/conceptql/database.rb
+++ lib/conceptql/database.rb
@@ -1,4 +1,5 @@
 require 'facets/hash/revalue'
+require 'facets/hash/symbolize_keys'
 
 module ConceptQL
   class Database
@@ -14,7 +15,7 @@ module ConceptQL
         db_type = db.database_type.to_sym
       end
 
-      @opts = opts.revalue { |v| v ? v.to_sym : v }
+      @opts = opts.revalue { |v| v ? v.to_sym : v }.symbolize_keys
       @opts[:data_model] ||= :omopv4
       @opts[:database_type] ||= db_type
       db.set(db_opts) if db.respond_to?(:set)
@@ -25,17 +26,18 @@ module ConceptQL
     end
 
     def db_opts
-      db_opts = {}
-      if opts[:database_type] == :impala
-        p ENV
-        if runtime_filter_mode = (opts[:impala_runtime_filter_mode] || ENV['IMPALA_RUNTIME_FILTER_MODE'])
-          db_opts.merge!(runtime_filter_mode: runtime_filter_mode)
-        end
-        if mem_limit = (opts[:impala_mem_limit] || ENV['IMPALA_MEM_LIMIT'])
-          db_opts.merge!(mem_limit: mem_limit)
-        end
+      opt_regexp = /^#{opts[:database_type]}_db_opt_/i
+
+      env_hash = ENV.to_hash.rekey { |k| k.to_s.downcase }
+      opts_hash = opts.rekey { |k| k.to_s.downcase }
+      all_opts = env_hash.merge(opts_hash)
+
+      matching_opts = all_opts.select { |k, _| k.match(opt_regexp) }
+
+      matching_opts.each_with_object({}) do |(k,v), h|
+        new_key = k.sub(opt_regexp, '')
+        h[new_key] = v
       end
-      db_opts
     end
 
     def extensions
