b4ca5eb986269ea00c624353d20eda7c594c9412
Ryan Duryea <aguynamedryan@gmail.com>
2017-06-23 14:19:10 -0700

    Database: Move DB_OPT processing to Sequelizer
    Addresses #120

--- lib/conceptql/database.rb
+++ lib/conceptql/database.rb
@@ -1,6 +1,3 @@
-require 'facets/hash/revalue'
-require 'facets/hash/symbolize_keys'
-
 module ConceptQL
   class Database
     attr :db, :opts
@@ -15,34 +12,16 @@ module ConceptQL
         db_type = db.database_type.to_sym
       end
 
-      @opts = opts.revalue { |v| v ? v.to_sym : v }.symbolize_keys
+      # Symbolize all keys and values
+      @opts = Hash[opts.map { |k,v| [k.to_sym, v ? v.to_sym : v]}]
+
       @opts[:data_model] ||= (ENV["CONCEPTQL_DATA_MODEL"] || :omopv4).to_sym
       @opts[:database_type] ||= db_type
-      db.set(db_opts) if db.respond_to?(:set)
     end
 
     def query(statement, opts={})
       NullQuery.new if statement.nil? || statement.empty?
       Query.new(db, statement, @opts.merge(opts))
-    end
-
-    def db_opts
-      opt_regexp = /^#{opts[:database_type]}_db_opt_/i
-
-      env_hash = ENV.to_hash.rekey { |k| k.to_s.downcase }
-      opts_hash = opts.rekey { |k| k.to_s.downcase }
-      all_opts = env_hash.merge(opts_hash)
-
-      matching_opts = all_opts.select { |k, _| k.match(opt_regexp) }
-
-      matching_opts.each_with_object({}) do |(k,v), h|
-        new_key = k.sub(opt_regexp, '')
-        h[new_key] = prep_value(k, v)
-      end
-    end
-
-    def prep_value(k, v)
-      v =~ /\W/ ? %Q|"#{v}"| : v
     end
 
     def extensions
