615a45d6c354db7113652a6eed2384d88f63c7d8
Ryan Duryea <aguynamedryan@gmail.com>
2017-06-27 16:16:39 -0700

    Add InformationPeriods operator (addresses #79)

---
+++ lib/conceptql/operators/information_periods.rb
@@ -0,0 +1,22 @@
+require_relative "operator"
+
+module ConceptQL
+  module Operators
+    class InformationPeriods < Operator
+      register __FILE__
+
+      desc 'Generates all observation_period records, or, if fed a stream, fetches all observation_period records for the people represented in the incoming result set.'
+      domains :observation_period
+      validate_no_upstreams
+
+      def query(db)
+        db[source_table]
+      end
+
+      def source_table
+        dm.table_by_domain(domain)
+      end
+    end
+  end
+end
+
