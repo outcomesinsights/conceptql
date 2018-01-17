9f56b6a37308bc7dcf2a54394b0a12f405ca1066
Ryan Duryea <aguynamedryan@gmail.com>
2018-01-17 11:56:03 -0800

    DM:OmopV4Plus#concepts_ds now uses more vanilla SQL to avoid Impala limitation

--- test/lib/conceptql/data_model/omopv4_plus_test.rb
+++ test/lib/conceptql/data_model/omopv4_plus_test.rb
@@ -1,4 +1,5 @@
 require_relative "../../../helper"
+require_relative "../../../db_helper"
 
 describe ConceptQL::DataModel::Omopv4Plus do
   describe "#related_concept_ids" do
@@ -8,6 +9,10 @@ describe ConceptQL::DataModel::Omopv4Plus do
 
     it "should return concept IDs exactly the same as those fed to it" do
       dm.related_concept_ids(nil, 8532).sort.must_equal [8532].sort
+    end
+
+    it "should return a single line per code, even if there are multiple descriptions available" do
+      dm.concepts_ds(DB, [4,5], "99214").to_a.must_equal [{:vocabulary_id=>4, :concept_code=>"99214", :min=>"Level 4 outpatient visit for evaluation and management of established patient with problem of moderate to high severity, including detailed history and medical decision making of moderate complexity - typical time with patient and/or family 25 minutes"}]
     end
   end
 end
