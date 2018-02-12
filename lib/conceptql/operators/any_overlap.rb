3d1849a8801e94c5e368f5b577a451fcce27573d
Ryan Duryea <aguynamedryan@gmail.com>
2018-02-11 23:21:42 -0800

    AnyOverlap: allow invert_match option

--- lib/conceptql/operators/any_overlap.rb
+++ lib/conceptql/operators/any_overlap.rb
@@ -8,7 +8,11 @@ module ConceptQL
       desc 'If a result in the LHR overlaps in any way a result in the RHR, it is passed through.'
       def apply_where_clause(ds)
         clause = ((within_start <= l_start_date) & (l_start_date <= within_end)) | ((l_start_date <= within_start) & (within_start <= l_end_date))
-        ds.where(clause)
+        if options[:invert_match]
+          ds.exclude(clause)
+        else
+          ds.where(clause)
+        end
       end
     end
   end
