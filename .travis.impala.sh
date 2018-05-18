296c4722beb21a8217171dc00b648b49831ff12a
Ryan Duryea <aguynamedryan@gmail.com>
2018-05-18 12:24:39 -0700

    Travis: a little more work on impala script

--- .travis.impala.sh
+++ .travis.impala.sh
@@ -8,10 +8,7 @@ eval "$(ssh-agent -s)"
 ssh-add <(echo "${IMPALA_CLUSTER_PRIVATE_KEY_BASE64}" | base64 --decode)
 ssh-add -l
 
-ssh -v -o "StrictHostKeyChecking=no" ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com ping -c 1 nonworker1.hadoop.jsaw.io
-if [ $? -ne 0 ]; then
-  echo "Cluster isn't up...skipping..."
-  exit 0
-fi
+ssh -v -o "StrictHostKeyChecking=no" ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com ping -c 1 nonworker1.hadoop.jsaw.io || exit 0
 
+echo "WOULD RUN TESTS"
 set +x
