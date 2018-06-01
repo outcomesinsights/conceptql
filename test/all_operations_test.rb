a6ef0bae1085958973057725cf0c5e06dee63d0b
Ryan Duryea <aguynamedryan@gmail.com>
2018-06-01 15:37:07 -0700

    Track time elapsed for each test

--- test/all_operations_test.rb
+++ test/all_operations_test.rb
@@ -6,6 +6,47 @@ if !argv.empty?
   file_regexps = argv.map { |f| /#{f}/ }
 end
 
+def my_time_it(name, &block)
+  start_time = Time.now
+  yield
+  return unless ENV["CONCEPTQL_TIME_IT"]
+  end_time = Time.now
+  CSV.open("/tmp/conceptql_times.csv", "a") do |csv|
+    csv << [name, start_time, end_time, end_time - start_time]
+  end
+end
+
+def run_test(f, basename)
+  test_type = basename.split("_").first
+
+  case test_type
+  when "crit"
+    criteria_ids(f)
+  when "anno"
+    annotate(f)
+  when "count"
+    criteria_counts(f)
+  when "optcc"
+    optimized_criteria_counts(f)
+  when "scanno"
+    scope_annotate(f)
+  when "domains"
+    domains(f)
+  when "cc"
+    criteria_counts(f)
+  when "optcc"
+    optimized_criteria_counts(f)
+  when "num"
+    numeric_values(f)
+  when "codes"
+    code_check(f)
+  when "results"
+    results(f)
+  else
+    raise "Invalid operation test prefix: #{test_type}"
+  end
+end
+
 describe ConceptQL::Operators do
 
   Dir['./test/statements/**/*'].each do |f|
@@ -15,33 +56,8 @@ describe ConceptQL::Operators do
     basename = File.basename(f)
 
     it "should produce correct results for #{f}" do
-      test_type = basename.split("_").first
-
-      case test_type
-      when "crit"
-        criteria_ids(f)
-      when "anno"
-        annotate(f)
-      when "count"
-        criteria_counts(f)
-      when "optcc"
-        optimized_criteria_counts(f)
-      when "scanno"
-        scope_annotate(f)
-      when "domains"
-        domains(f)
-      when "cc"
-        criteria_counts(f)
-      when "optcc"
-        optimized_criteria_counts(f)
-      when "num"
-        numeric_values(f)
-      when "codes"
-        code_check(f)
-      when "results"
-        results(f)
-      else
-        raise "Invalid operation test prefix: #{test_type}"
+      my_time_it(f) do
+        run_test(f, basename)
       end
     end
   end
