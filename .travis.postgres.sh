d36108d52109116d98c063cb89ec16da16d49e17
Ryan Duryea <aguynamedryan@gmail.com>
2018-09-06 09:19:12 -0700

    Travis: use better way to load test_data_for_jigsaw

--- .travis.postgres.sh
+++ .travis.postgres.sh
@@ -4,8 +4,7 @@ set -x
 
 echo "${SEQUELIZER_URI}" | grep -i postgres || exit 0
 
-createdb test_data_for_jigsaw
-curl -sSL "http://test_data_for_jigsaw.jsaw.io" | pigz -dc | psql postgres://postgres@localhost/test_data_for_jigsaw > /tmp/restore.log 2>&1 || cat /tmp/restore.log
+curl -sSL "http://test_data_for_jigsaw.jsaw.io" | pigz -dc | psql postgres://postgres@localhost/postgres > /tmp/restore.log 2>&1 || cat /tmp/restore.log
 df -h
 
 bundle exec ruby test/all.rb
