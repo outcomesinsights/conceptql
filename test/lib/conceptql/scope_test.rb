8e96272fae58da34b05570a3a64387a8e6199f2d
Ryan Duryea <aguynamedryan@gmail.com>
2019-01-03 15:27:57 -0800

    Update a few more tests for scope windows

--- test/lib/conceptql/scope_test.rb
+++ test/lib/conceptql/scope_test.rb
@@ -94,12 +94,12 @@ describe ConceptQL::Scope do
 
       it "should limit selection on condition_occurrence table" do
         db = ConceptQL::Database.new(Sequel.mock(host: host), data_model: :omopv4_plus)
-        db.query(["visit_occurrence", true], opts).sql.must_match(/EXISTS/)
+        db.query(["visit_occurrence", true], opts).sql.must_match(/INNER JOIN/)
       end
 
       it "should not apply to inner query of revenue code operator" do
         db = ConceptQL::Database.new(Sequel.mock(host: host), data_model: :omopv4_plus)
-        db.query(["revenue_code", "0450"], opts).sql.downcase.scan("exists").count.must_equal 1
+        db.query(["revenue_code", "0450"], opts).sql.downcase.scan("inner join").count.must_equal 1
       end
     end
 
