514bc510bd415bbaab721ab887b6db1439132314
Ryan Duryea <aguynamedryan@gmail.com>
2019-01-18 16:42:51 -0800

    Travis: try to stay under 50 minute limit

--- test/helper.rb
+++ test/helper.rb
@@ -23,3 +23,29 @@ require 'minitest/autorun'
 
 Minitest::Test.make_my_diffs_pretty!
 
+class Minitest::HooksSpec
+  if ENV['TESTS_DIV_MOD']
+    div, mod = ENV['TESTS_DIV_MOD'].split(' ', 2).map(&:to_i)
+    raise "invalid TESTS_DIV_MOD div: #{div}" unless div >= 2
+    raise "invalid TESTS_DIV_MOD mod: #{mod} (div: #{div})" unless mod >= 0 && mod < div
+    test_number = -1
+    inc = proc{test_number+=1}
+
+    define_singleton_method(:it) do |*a, &block|
+      if inc.call % div == mod
+        super(*a, &block)
+      end
+    end
+  end
+
+  def log
+    begin
+      DB.loggers << Logger.new(STDOUT)
+      yield
+    ensure
+     DB.loggers.pop
+    end
+  end
+end
+
+
