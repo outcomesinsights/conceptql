bcee74727c778d3843e692b869e3dee4c047b437
Jeremy Evans <code@jeremyevans.net>
2019-01-21 19:16:49 -0800

    Fix TESTS_DIV_MOD support to split up test suite
    ConceptQL doesn't use minitest-hooks, as it operates on read-only
    data.  So instead of Minitest::HooksSpec, use Minitest::Spec
    and prepend a module to the singleton class than will ignore
    calls to "it" if they aren't included by the TESTS_DIV_MOD
    setting.

--- test/helper.rb
+++ test/helper.rb
@@ -23,3 +23,20 @@ require 'minitest/autorun'
 
 Minitest::Test.make_my_diffs_pretty!
 
+class Minitest::Spec
+  if ENV['TESTS_DIV_MOD']
+    div, mod = ENV['TESTS_DIV_MOD'].split(' ', 2).map(&:to_i)
+    raise "invalid TESTS_DIV_MOD div: #{div}" unless div >= 2
+    raise "invalid TESTS_DIV_MOD mod: #{mod} (div: #{div})" unless mod >= 0 && mod < div
+    test_number = -1
+    inc = proc{test_number+=1}
+
+    singleton_class.prepend(Module.new do
+      define_method(:it) do |*a, &block|
+        if (i = inc.call) % div == mod
+          super(*a, &block)
+        end
+      end
+    end)
+  end
+end
