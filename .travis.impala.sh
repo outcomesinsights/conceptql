738df83e590c4ff5ecf606fe93fd699e7294ee84
Ryan Duryea <aguynamedryan@gmail.com>
2019-01-25 08:24:28 -0800

    Travis: reduce output of SSH

--- .travis.impala.sh
+++ .travis.impala.sh
@@ -10,10 +10,13 @@ ssh-add -l
 
 echo "Added key..."
 
-ssh -v -o "StrictHostKeyChecking=no" ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com ping -c 1 nonworker2.hadoop.jsaw.io || exit 0
+#ssh_args="-v"
+ssh_args=""
+
+ssh "${ssh_args}" -o "StrictHostKeyChecking=no" ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com ping -c 1 nonworker2.hadoop.jsaw.io || exit 0
 echo "nonworker is up..."
 
-ssh -v -o "StrictHostKeyChecking=no" -M -S my-ctrl-socket -fnNT -L 21000:nonworker2.hadoop.jsaw.io:21000 ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com
+ssh "${ssh_args}" -o "StrictHostKeyChecking=no" -M -S my-ctrl-socket -fnNT -L 21000:nonworker2.hadoop.jsaw.io:21000 ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com
 echo "Tunnel is up..."
 
 bundle exec ruby test/all.rb
