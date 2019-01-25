19be95c0af62d558b6fbd38d96007cdde4c43985
Ryan Duryea <aguynamedryan@gmail.com>
2019-01-24 17:18:17 -0800

    Travis: more attempts to get stalled tests running

--- .travis.impala.sh
+++ .travis.impala.sh
@@ -8,8 +8,13 @@ eval "$(ssh-agent -s)"
 timeout 10 ssh-add <(echo "${IMPALA_CLUSTER_PRIVATE_KEY_BASE64}" | base64 --decode)
 ssh-add -l
 
-ssh -v -o "StrictHostKeyChecking=no" ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com ping -c 1 nonworker1.hadoop.jsaw.io || exit 0
-ssh -v -o "StrictHostKeyChecking=no" -M -S my-ctrl-socket -fnNT -L 21000:nonworker1.hadoop.jsaw.io:21000 ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com
+echo "Added key..."
+
+ssh -v -o "StrictHostKeyChecking=no" ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com ping -c 1 nonworker2.hadoop.jsaw.io || exit 0
+echo "nonworker is up..."
+
+ssh -v -o "StrictHostKeyChecking=no" -M -S my-ctrl-socket -fnNT -L 21000:nonworker2.hadoop.jsaw.io:21000 ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com
+echo "Tunnel is up..."
 
 bundle exec ruby test/all.rb
 result=$?
