04a7dda32c21f59424e3dd66f5a086612dbe8573
Ryan Duryea <aguynamedryan@gmail.com>
2019-01-27 07:52:10 -0800

    Travis: timeout SSH/sequel_impala more quickly

--- .travis.impala.sh
+++ .travis.impala.sh
@@ -8,8 +8,10 @@ eval "$(ssh-agent -s)"
 ssh-add <(echo "${IMPALA_CLUSTER_PRIVATE_KEY_BASE64}" | base64 --decode)
 ssh-add -l
 
-ssh -v -o "StrictHostKeyChecking=no" ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com ping -c 1 nonworker1.hadoop.jsaw.io || exit 0
-ssh -v -o "StrictHostKeyChecking=no" -M -S my-ctrl-socket -fnNT -L 21000:nonworker1.hadoop.jsaw.io:21000 ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com
+ssh -o "ConnectTimeout=20" -o "StrictHostKeyChecking=no" ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com ping -c 1 nonworker1.hadoop.jsaw.io || exit 0
+ssh -o "ConnectTimeout=20" -o "StrictHostKeyChecking=no" -M -S my-ctrl-socket -fnNT -L 21000:nonworker1.hadoop.jsaw.io:21000 ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com
+
+SEQUELIZER_TIMEOUT=30
 
 bundle exec ruby test/all.rb
 result=$?
