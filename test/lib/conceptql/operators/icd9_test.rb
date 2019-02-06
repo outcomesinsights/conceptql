a4663e7fe0bcbaf718327a1fba3624f7301166c1
Ryan Duryea <aguynamedryan@gmail.com>
2019-02-06 15:14:21 -0800

    Operator#fix_arguments: use proper CTE name for arguments_fix
    Refs #217

---
+++ test/lib/conceptql/operators/icd9_test.rb
@@ -0,0 +1,21 @@
+require_relative "../../../helper"
+
+describe ConceptQL::Operators::Icd9 do
+  it "should use unique names for 'args table' under Impala" do
+    db = ConceptQL::Database.new(Sequel.mock(host: :impala), data_model: :omopv4_plus, force_temp_tables: true, scratch_database: "jigsaw_temp")
+    values = (1..11_000).to_a
+    stmt = [ :union,
+      [:icd9, *values],
+      [:icd9, *values]
+    ]
+
+    sql = db.query(stmt).sql(:create_tables)
+    match_data = sql.scan(/(args_[^`]+)/).flatten
+    match_data[0].must_match(/args_\d+/)
+    match_data[1].must_match(/args_\d+/)
+    match_data[0].wont_equal(match_data[1])
+  end
+end
+
+
+
