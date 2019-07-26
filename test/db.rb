7abd58f02daf57a7ea73ae734fee579b7c7be4a6
Ryan Duryea <aguynamedryan@gmail.com>
2019-07-26 12:41:30 -0700

    Warn when running tests without LEXICON_URL set
    I just lost 2 hours to an error because I didn't have Lexicon
    set up.  So, now there's a warning if you're running tests without
    Lexicon.

--- test/db.rb
+++ test/db.rb
@@ -24,3 +24,14 @@ END
   exit 1
 end
 
+unless ENV["LEXICON_URL"]
+  $stderr.puts <<END
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+Looks like you're running tests without LEXICON_URL set.
+
+I'ma let you finish, but just know that you're probably going
+run into some failing tests because they require Lexicon.
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+END
+end
