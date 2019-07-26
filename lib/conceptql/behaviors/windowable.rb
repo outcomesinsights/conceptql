255d6df94751cab232f82fdce38a55169ea9b3fa
Ryan Duryea <aguynamedryan@gmail.com>
2019-07-26 13:05:00 -0700

    Windowable: more refactoring

--- lib/conceptql/behaviors/windowable.rb
+++ lib/conceptql/behaviors/windowable.rb
@@ -1,12 +1,24 @@
 module ConceptQL
   module Behaviors
+    # Applies scope windows to Operators
     module Windowable
       def select_it(query, specific_table = nil)
         ds = super
-        return ds if respond_to?(:skip_windows?) && skip_windows?
-        timeless = respond_to?(:timeless?) && timeless?
-        scope.windows.each{|w| ds = w.call(self, ds, timeless: timeless)}
+        return ds if skip_windows?
+
+        scope.windows.each do |window|
+          ds = window.call(self, ds, timeless: timeless?)
+        end
+
         ds
+      end
+
+      def skip_windows?
+        false
+      end
+
+      def timeless?
+        false
       end
     end
   end
