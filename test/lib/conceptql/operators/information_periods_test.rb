66dd49c49c0460a0a571671337af333723eee060
Ryan Duryea <aguynamedryan@gmail.com>
2020-01-17 19:01:11 +0000

    Get InformationPeriods tests passing

--- test/lib/conceptql/operators/information_periods_test.rb
+++ test/lib/conceptql/operators/information_periods_test.rb
@@ -2,22 +2,13 @@ require_relative "../../../helper"
 require "conceptql"
 
 describe ConceptQL::Operators::Selection::InformationPeriods do
-  it "should appear for both GDM" do
-    _(ConceptQL::Operators.operators[:gdm]["information_periods"]).must_equal ConceptQL::Operators::InformationPeriods
-  end
-
-  it "should appear for both OMOPv4+" do
-    _(ConceptQL::Operators.operators[:omopv4_plus]["information_periods"]).must_equal ConceptQL::Operators::InformationPeriods
+  it "should appear for GDM" do
+    _(ConceptQL::Operators.operators[:gdm]["information_periods"]).must_equal ConceptQL::Operators::Selection::InformationPeriods
   end
 
   it "should produce correct SQL under gdm" do
-    db = ConceptQL::Database.new(Sequel.mock(host: :postgres), data_model: :gdm)
-    _(db.query(["information_periods"]).sql).must_equal "SELECT * FROM (SELECT * FROM (SELECT \"patient_id\" AS \"person_id\", \"id\" AS \"criterion_id\", CAST('information_periods' AS text) AS \"criterion_table\", CAST('observation_period' AS text) AS \"criterion_domain\", \"start_date\", \"end_date\", CAST(NULL AS text) AS \"source_value\", CAST(NULL AS text) AS \"source_vocabulary_id\" FROM \"information_periods\") AS \"t1\") AS \"t1\""
-  end
-
-  it "should produce correct SQL under omopv4_plus" do
-    db = ConceptQL::Database.new(Sequel.mock(host: :postgres), data_model: :omopv4_plus)
-    _(db.query(["information_periods"]).sql).must_equal "SELECT * FROM (SELECT * FROM (SELECT \"person_id\" AS \"person_id\", \"observation_period_id\" AS \"criterion_id\", CAST('observation_period' AS text) AS \"criterion_table\", CAST('observation_period' AS text) AS \"criterion_domain\", CAST(\"observation_period_start_date\" AS date) AS \"start_date\", CAST(coalesce(\"observation_period_end_date\", \"observation_period_start_date\") AS date) AS \"end_date\", CAST(NULL AS text) AS \"source_value\", CAST(NULL AS text) AS \"source_vocabulary_id\" FROM \"observation_period\") AS \"t1\") AS \"t1\""
+    cdb = ConceptQL::Database.new(Sequel.mock(host: :postgres), data_model: :gdm)
+    sql_matches(cdb.query(["information_periods"]).sql, "information_periods_cql_view")
   end
 end
 
