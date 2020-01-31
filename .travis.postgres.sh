2ced8fefd6d294eb2193d593445164a3f26e2513
Ryan Duryea <aguynamedryan@gmail.com>
2020-01-31 23:12:41 +0000

    Switch back to chisel.test_data.jsaw.io for test data

--- .travis.postgres.sh
+++ .travis.postgres.sh
@@ -4,7 +4,7 @@ set -x
 
 echo "${SEQUELIZER_URI}" | grep -i postgres || exit 0
 
-time curl -sSL "http://www.dropbox.com/s/ioj1mqc76o05lbe/test_data_for_chisel.sql.gz?dl=1" | pigz -dc | psql postgres://postgres@localhost/postgres > /tmp/restore.log 2>&1 || cat /tmp/restore.log
+time curl -sSL "http://chisel.test_data.jsaw.io" | pigz -dc | psql postgres://postgres@localhost/postgres > /tmp/restore.log 2>&1 || cat /tmp/restore.log
 
 # Spot check OMOPv4+
 #for table in person condition_occurrence procedure_occurrence observation observation period; do
---
+++ test/lib/conceptql_test.rb
@@ -0,0 +1,7 @@
+require_relative "../helper"
+
+describe ConceptQL do
+  it "should produce metadata" do
+    ConceptQL.metadata
+  end
+end
