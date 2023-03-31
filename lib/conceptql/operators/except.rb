b282fef761daa53065e93d01f8d0b8f3089c8fed
Ryan Duryea <aguynamedryan@gmail.com>
2023-03-31 10:04:01 -0700

    Prepare Except for new column behavior
    We were using the EXCEPT operator from SQL
    but we want to make sure not to include the new
    `label` column because that is metadata
    and not real data to be used in filtering the
    results from a ConceptQL statement

--- lib/conceptql/operators/except.rb
+++ lib/conceptql/operators/except.rb
@@ -9,22 +9,18 @@ module ConceptQL
       default_query_columns
 
       def query(db)
-        if ignore_dates?
-          query = db.from(Sequel.as(left.evaluate(db), :l))
-            .left_join(Sequel.as(right.evaluate(db), :r), criterion_id: :criterion_id, criterion_domain: :criterion_domain)
-            .where(Sequel[:r][:criterion_id] => nil)
-            .select_all(:l)
-          db.from(query)
-        else
-          lquery = left.evaluate(db)
-          rquery = right.evaluate(db)
+        cols_to_compare = [
+          :criterion_id,
+          :criterion_table,
+        ]
+        cols_to_compare += [:start_date, :end_date] unless ignore_dates?
+        join_cols = cols_to_compare.zip(cols_to_compare).to_h
 
-          # Set columns so that impala's EXCEPT emulation doesn't use a query to determine them
-          lquery.send("columns=", query_cols)
-          rquery.send("columns=", query_cols)
-
-          lquery.select(*query_cols).except(rquery.select(*query_cols))
-        end
+        query = db.from(Sequel.as(left.evaluate(db), :l))
+          .left_join(Sequel.as(right.evaluate(db), :r), join_cols)
+          .where(Sequel[:r][:criterion_id] => nil)
+          .select_all(:l)
+        db.from(query)
       end
 
       private
