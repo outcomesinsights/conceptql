07648fb29cdaaa2ad9bc949382eaa465b9bb39df
Ryan Duryea <aguynamedryan@gmail.com>
2023-11-28 10:53:42 -0800

    Implement Spark#days_between, refs #285

--- lib/conceptql/rdbms/spark.rb
+++ lib/conceptql/rdbms/spark.rb
@@ -3,10 +3,13 @@ require_relative "generic"
 module ConceptQL
   module Rdbms
     class Spark < Generic
+      def days_between(from_column, to_column)
+        Sequel.function(:datediff, cast_date(to_column), cast_date(from_column))
+      end
+
       def preferred_formatter
         SqlFormatters::PgFormat
       end
     end
   end
 end
-
