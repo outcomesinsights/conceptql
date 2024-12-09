248e1a73d3c01ed2381dcef49d9c3c7de8ff4843
Ryan Duryea <aguynamedryan@gmail.com>
2024-12-09 10:20:34 -0800

    Add cold_col Sequel extension

---
+++ lib/sequel/extensions/cold_col.rb
@@ -0,0 +1,137 @@
+# frozen-string-literal: true
+
+module Sequel
+  module ColdColDatabase
+    def self.extended(db)
+      db.extend_datasets(ColdColDataset)
+    end
+
+    def load_schema(path)
+      schemas = Psych.load_file(path).map do |table, info|
+        columns = info[:columns].map { |column_name, col_info| [column_name, col_info] }
+        [literal(table), columns]
+      end.to_h
+      schemas = (instance_variable_get(:@schemas) || {}).merge(schemas)
+      instance_variable_set(:@schemas, schemas)
+    end
+
+    def add_table_schema(name, info)
+      instance_variable_get(:@schemas)[literal(name)] = info
+    end
+  end
+
+  module ColdColDataset
+    def columns
+      columns_search
+    end
+
+    def columns_search(opts_chain = nil)
+      if cols = _columns
+        return cols
+      end
+
+      unless (pcs = probable_columns(opts.merge(parent_opts: opts_chain))) && pcs.all?
+        raise("Failed to find columns for #{sql}")
+      end
+
+      self.columns = pcs
+    end
+
+    protected
+
+    WILDCARD = Sequel.lit('*').freeze
+
+    def probable_columns(opts_chain)
+      if !(cols = opts[:select]).blank?
+        from_stars = []
+
+        if has_select_all?(cols)
+          from_stars = (opts[:from] || []).flat_map { |from| fetch_columns(from, opts_chain) }
+          cols = cols.reject { |c| c == WILDCARD }
+        end
+
+        from_stars += cols
+                      .select { |c| c.is_a?(Sequel::SQL::ColumnAll) }
+                      .flat_map { |c| from_named_sources(c.table.to_sym, opts_chain) }
+
+        cols = cols.reject { |c| c.is_a?(Sequel::SQL::ColumnAll) }
+
+        (from_stars + cols.map { |c| probable_column_name(c) }).flatten
+      else
+        froms = opts[:from] || []
+        joins = (opts[:join] || []).map { |jc| jc.table_expr }
+        (froms + joins).flat_map { |from| fetch_columns(from, opts_chain) }
+      end
+    end
+
+    private
+
+    def has_select_all?(cols)
+      cols.any? { |c| c == WILDCARD }
+    end
+
+    def from_named_sources(name, opts_chain)
+      schemas = db.instance_variable_get(:@schemas)
+
+      current_opts = opts_chain
+
+      from = (opts[:from] || [])
+             .select { |f| f.is_a?(Sequel::SQL::AliasedExpression) }
+             .detect { |f| literal(f.alias) == literal(name) }
+
+      return from.expression.columns_search(opts_chain) if from
+
+      with = nil
+
+      while current_opts.present? && with.blank?
+        with = (current_opts[:with] || []).detect { |wh| literal(wh[:name]) == literal(name) }
+        current_opts = current_opts[:parent_opts]
+      end
+
+      # if (join = (opts[:join] || []).detect { |jc| literal(jc.alias) == literal(name) })
+      #  jc.table_expr.columns
+      # els
+
+      return with[:dataset].columns_search(opts_chain) if with
+
+      if schemas && (table = literal(name)) && (sch = Sequel.synchronize { schemas[table] })
+        return sch.map { |c, _| c }
+      end
+
+      pp name
+      pp opts_chain
+      pp sql
+      raise("Failed to find columns for #{name}")
+    end
+
+    def fetch_columns(from, opts_chain)
+      from = from.expression if from.is_a?(SQL::AliasedExpression)
+
+      case from
+      when Dataset
+        from.columns_search(opts_chain)
+      when Symbol, SQL::Identifier, SQL::QualifiedIdentifier
+        from_named_sources(from, opts_chain)
+      end
+    end
+
+    # Return the probable name of the column, or nil if one
+    # cannot be determined.
+    def probable_column_name(c)
+      case c
+      when Symbol
+        _, c, a = split_symbol(c)
+        (a || c).to_sym
+      when SQL::Identifier
+        c.value.to_sym
+      when SQL::QualifiedIdentifier
+        c.column.to_sym
+      when SQL::AliasedExpression
+        a = c.alias
+        a.is_a?(SQL::Identifier) ? a.value.to_sym : a.to_sym
+      end
+    end
+  end
+
+  Database.register_extension(:cold_col, Sequel::ColdColDatabase)
+end
---
+++ test/lib/sequel/extensions/cold_col_test.rb
@@ -0,0 +1,90 @@
+require_relative '../../../helper'
+require_relative '../../../../lib/sequel/extensions/cold_col'
+
+describe Sequel::ColdColDatabase do
+  let(:db) do
+    @db = Sequel.mock.extension(:cold_col)
+    @db.instance_variable_set(:@schemas, {
+                                Sequel.lit('tab1') => [[:col1]],
+                                Sequel.lit('tab2') => [[:col2]],
+                                Sequel.lit('tab3') => [[:col3], [:col4]]
+                              })
+    @db.extend_datasets do
+      def supports_cte?
+        true
+      end
+    end
+    @db
+  end
+
+  def expect_columns(ds, *cols)
+    _(ds.columns).must_equal(cols)
+  end
+
+  it 'should know columns from select * FROM tab' do
+    expect_columns(db[:tab1], :col1)
+  end
+
+  it 'should know columns after append' do
+    expect_columns(db[:tab1].select_append(Sequel.function(:min, :col1).as(:mini)), :col1, :mini)
+  end
+
+  it 'should know columns after select_all' do
+    expect_columns(db[:tab1].select_all, :col1)
+  end
+
+  it 'should know columns after select_all(:tab1)' do
+    expect_columns(db[:tab1].select_all(:tab1), :col1)
+  end
+
+  it 'should know columns after from_self' do
+    expect_columns(db[:tab1].from_self, :col1)
+  end
+
+  it 'should know columns after a CTE' do
+    ds = db[:cte1]
+         .with(:cte1, db[:tab1])
+    expect_columns(ds, :col1)
+  end
+
+  it 'should know columns after a JOIN' do
+    ds = db[:tab1]
+         .join(:tab2)
+    expect_columns(ds, :col1, :col2)
+  end
+
+  it 'should know columns after a different kind of JOIN' do
+    ds = db[:tab1]
+         .join(db[:tab2])
+    expect_columns(ds, :col1, :col2)
+  end
+
+  it 'should know columns from a JOIN and CTE' do
+    ds = db[:tab1]
+         .with(:cte1, db[:tab2])
+         .join(db[:cte1])
+    expect_columns(ds, :col1, :col2)
+  end
+
+  it 'should know columns from a select_all JOIN' do
+    ds = db[:tab1]
+         .join(db[:tab2], { Sequel[:tab1][:col1] => Sequel[:tab2][:col3] })
+         .select_all(:tab1)
+    expect_columns(ds, :col1)
+  end
+
+  it 'should know columns from an aliased select_all JOIN' do
+    ds = db[:tab1].from_self(alias: :l)
+                  .join(db[:tab2], { col3: :col1 })
+                  .select_all(:l)
+    expect_columns(ds, :col1)
+  end
+
+  it 'should know columns from an aliased select_all JOIN' do
+    ds = db[:tab1].from_self(alias: :l)
+                  .join(db[:tab2], { col3: :col1 }, table_alias: :r)
+                  .select_all(:l)
+                  .select_append(Sequel[:r][:col4])
+    expect_columns(ds, :col1, :col4)
+  end
+end
