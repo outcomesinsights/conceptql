2f3456b157107f87ba1fbaf8c30817a6920b3b89
Ryan Duryea <aguynamedryan@gmail.com>
2026-03-17 21:11:38 -0700

    Add DuckDB test setup path

--- test/db_helper.rb
+++ test/db_helper.rb
@@ -14,6 +14,15 @@ if DB.database_type.to_sym == :spark && ENV['CONCEPTQL_PARQUET_TEST_DIR'].presen
   DB.make_ready(search_path: Dir[ENV['CONCEPTQL_PARQUET_TEST_DIR']].map { |f| Pathname.new(f).glob('*.parquet') })
 end
 
+if DB.database_type.to_sym == :duckdb
+  schemas = (ENV['SEQUELIZER_SEARCH_PATH'] || 'slim,gdm_vocabs,ohdsi_vocabs')
+            .split(',')
+            .map(&:strip)
+            .reject(&:empty?)
+            .map(&:to_sym)
+  DB.make_ready(search_path: schemas)
+end
+
 CDB = ConceptQL::Database.new(DB, data_model: (ENV['CONCEPTQL_DATA_MODEL'] || ConceptQL::DEFAULT_DATA_MODEL).to_sym)
 DB.extension :error_sql
 
